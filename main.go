39ee8105fc47d5b47ad782ebd0aa998ca6494a8a
Fábio Fernandes <dev.fabiofernandes@gmail.com>
2025-02-17 12:05:47 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"pizzaria/models"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+var pizzas []models.Pizza
+
+func main() {
+	loadPizzas()
+	r := gin.Default()
+	r.GET("/pizzas", getPizzas)
+	r.POST("/pizzas", postPizzas)
+	r.GET("/pizzas/:id", getPizzasByID)
+	r.Run()
+}
+
+func getPizzas(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"pizzas": pizzas,
+	})
+}
+
+func postPizzas(c *gin.Context) {
+	var newPizza models.Pizza
+	if err := c.ShouldBindJSON(&newPizza); err != nil {
+		c.JSON(400, gin.H{
+			"erro": "deu ruim :((",
+		})
+	}
+	newPizza.ID = len(pizzas) + 1
+	pizzas = append(pizzas, newPizza)
+	savePizza()
+	c.JSON(201, newPizza)
+}
+
+func getPizzasByID(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"erro": err.Error(),
+		})
+		return
+	}
+	for _, p := range pizzas {
+		if p.ID == id {
+			c.JSON(200, p)
+			return
+		}
+	}
+	c.JSON(404, gin.H{
+		"message": "Pizza not found",
+	})
+}
+
+func loadPizzas() {
+	file, err := os.Open("data/pizza.json")
+	if err != nil {
+		fmt.Println("Error file:", err)
+		return
+	}
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&pizzas); err != nil {
+		fmt.Println("Error decoding JSON: ", err)
+	}
+}
+
+func savePizza() {
+	file, err := os.Create("data/pizza.json")
+	if err != nil {
+		fmt.Println("Error file:", err)
+		return
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	if err := encoder.Encode(pizzas); err != nil {
+		fmt.Println("Error encoding JSON:", err)
+	}
+
+}
